c0d2b51680ba0d9e7582e81dc111e55401f65e18
aline <alineporfirot@gmail.com>
2018-05-28 10:32:04 -0300

    c
    c

---
+++ src/main/java/br/com/eventomeioambiente/conexao/Conexao.java
@@ -0,0 +1,54 @@
+package br.com.eventomeioambiente.conexao;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JFileChooser;
+
+public class Conexao {
+	/**
+	 * @parameter expression="${encoding}" default-value="${project.build.sourceEncoding}"
+	 */
+	private String encoding;
+	private Connection con = null;
+
+	public Statement stmt;
+
+	public Connection conectar() {
+
+		String endereco = "jdbc:postgresql://localhost:5432/BDEventoMeioAmbiente";
+		String usuario = "postgres";
+		String senha = "admin";
+
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		}
+		try {
+			con = DriverManager.getConnection(endereco, usuario, senha);
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public void desconectar() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	
+}
---
+++ src/main/java/br/com/eventomeioambiente/controlle/AlunoBean.java
@@ -0,0 +1,80 @@
+package br.com.eventomeioambiente.controlle;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import br.com.eventomeioambiente.dao.AlunoDao;
+import br.com.eventomeioambiente.modelo.Aluno;
+
+
+@RequestScoped
+@ManagedBean (name = "alunoBean")
+public class AlunoBean implements Serializable{
+	/**
+	 * @parameter expression="${encoding}" default-value="${project.build.sourceEncoding}"
+	 */
+	private String encoding;
+	private static final long serialVersionUID = 1L;
+	
+	AlunoDao alunoDao;
+	private Aluno alunoBusca;
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	private Aluno aluno;
+	
+	@PostConstruct
+	public void init() {
+		alunoDao = new AlunoDao();
+		alunoBusca = new Aluno();
+		aluno = new Aluno();
+	
+	}//tiramos de usuarioBean
+
+	public String buscarAluno(){
+		try{
+			aluno = alunoDao.buscarAluno(alunoBusca.getMatricula(), alunoBusca.getSenha());
+			if(aluno != null){
+				FacesContext context = FacesContext.getCurrentInstance();
+				HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+				session.setAttribute("aluno", aluno);
+				return "/inicio";
+			}else{
+				FacesContext ctx = FacesContext.getCurrentInstance();
+				ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+					"Usuário ou Senha incorretos,tente novamente!", ""));
+				
+			}
+		}catch(Exception e){
+			FacesContext ctx = FacesContext.getCurrentInstance();
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+				"Erro ao autenticar o usuário", ""));
+			
+		}
+		return null;
+	}
+	public AlunoDao getAlunoDao() {
+		return alunoDao;
+	}
+	public void setAlunoDao(AlunoDao alunoDao) {
+		this.alunoDao = alunoDao;
+	}
+	public Aluno getAlunoBusca() {
+		return alunoBusca;
+	}
+	public void setAlunoBusca(Aluno alunoBusca) {
+		this.alunoBusca = alunoBusca;
+	}
+	public Aluno getAluno() {
+		return aluno;
+	}
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+}
